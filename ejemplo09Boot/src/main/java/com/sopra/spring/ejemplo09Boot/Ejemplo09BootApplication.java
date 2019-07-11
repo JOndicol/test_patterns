c1ef252e1a3aa6950b429a621af5b86eb973460f
Jorge Ondicol García <jorge.ondicol@gmail.com>
2019-07-11 10:31:38 +0200

    ejemplo springboot añadido

---
+++ ejemplo09Boot/src/main/java/com/sopra/spring/ejemplo09Boot/Ejemplo09BootApplication.java
@@ -0,0 +1,13 @@
+package com.sopra.spring.ejemplo09Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo09BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo09BootApplication.class, args);
+	}
+
+}
